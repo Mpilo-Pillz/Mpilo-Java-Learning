9e83d05a6a9f279460580cb9de0f30cb7481e95e
Fikasentani <mpilo.dlamini@yahoo.com>
2020-11-13 08:27:00 +0200

    adding house with constuctors

--- Adhoc-Arrays-Loops/src/ArraysNLoopsRunner.java
+++ Adhoc-Arrays-Loops/src/ArraysNLoopsRunner.java
@@ -2,27 +2,32 @@ public class ArraysNLoopsRunner {
     public static void main (String [] args) {
         ArraysNLoops arraysNLoops = new ArraysNLoops();
 
-//        arraysNLoops.printArray();
-//        arraysNLoops.numberOfItemsInArray;
 
-         Car car = new Car();
-         car.numberOfDoors = 4;
-         car.gearbox = "Automatic";
-         car.name = "Volvo";
-        car.color = "Black";
 
-        System.out.println("The " + car.name + " is " + car.color + " has " + car.gearbox + " transmission" + " and is a " + car.numberOfDoors + " doors ");
-        car.move("Simmonds");
+//         Car car = new Car();
+//         car.numberOfDoors = 4;
+//         car.gearbox = "Automatic";
+//         car.name = "Volvo";
+//        car.color = "Black";
+//
+//        System.out.println("The " + car.name + " is " + car.color + " has " + car.gearbox + " transmission" + " and is a " + car.numberOfDoors + " doors ");
+//        car.move("Simmonds");
+//
+//        Car carTwo = new Car();
+//        carTwo.numberOfDoors = 2;
+//        carTwo.gearbox = "Manual";
+//        carTwo.name = "Lexus";
+//        carTwo.color = "White";
+//
+//        System.out.println("The " + carTwo.name + " is " + carTwo.color + " has " + carTwo.gearbox + " transmission" + " and is a " + carTwo.numberOfDoors + " doors ");
+//        carTwo.move("Grayston");
+//
+//
+//        car.petrol = 16;
+//        car.arrivedAtDestition("no");
 
-        Car carTwo = new Car();
-        carTwo.numberOfDoors = 2;
-        carTwo.gearbox = "Manual";
-        carTwo.name = "Lexus";
-        carTwo.color = "White";
+        House house = new House("6 Bedroom", "Double Storey", "3 Bathrooms", 400.50, 4);
+        System.out.println(String.format("This is a %s %s house that has a size of %sm2 big enough to park %s cars and it has %s",house.numberOfBedrooms, house.houseType, house.squareMeters, house.garages, house.numberOfBathrooms) );
 
-        System.out.println("The " + carTwo.name + " is " + carTwo.color + " has " + carTwo.gearbox + " transmission" + " and is a " + carTwo.numberOfDoors + " doors ");
-        carTwo.move("Grayston");
-
-        car.createSalesPitch();
     }
 }
