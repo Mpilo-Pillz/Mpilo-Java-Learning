03ba016b6b9f5f5324620b99583d51ade7387dbc
mpilopillz <mpilo.dlamini@yahoo.com>
2023-07-24 12:12:45 +0200

    baking up dot com after not getting it to run

--- ChapterSix/src/DotCom.java
+++ ChapterSix/src/DotCom.java
@@ -2,11 +2,15 @@ import java.util.ArrayList;
 
 public class DotCom {
     private ArrayList<String> locationCells;
+    private String name;
 
     public void setLocationCells(ArrayList<String> loc) {
         locationCells = loc;
     }
 
+    public void setName(String n) {
+        name = n;
+    }
     public String checkYourSelf(String userInput) {
         String result = "miss";
 
