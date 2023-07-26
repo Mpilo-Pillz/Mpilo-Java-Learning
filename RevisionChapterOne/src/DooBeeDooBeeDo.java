671ab42e30368ccb6a76061f74442b18eb89d753
mpilopillz <mpilo.dlamini@yahoo.com>
2023-07-26 18:11:34 +0200

    revising do be exercise

---
+++ RevisionChapterOne/src/DooBeeDooBeeDo.java
@@ -0,0 +1,13 @@
+public class DooBeeDooBeeDo {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 4) {
+            System.out.print("Goo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 4) {
+            System.out.println("Do");
+        }
+    }
+}
