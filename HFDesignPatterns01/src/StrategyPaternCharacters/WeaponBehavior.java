39f4aa6071f2c9357010e29a17890ce9498a6796
Fikasentani <mpilo.dlamini@yahoo.com>
2023-09-30 13:55:01 +0200

    creating weapon behaviour

---
+++ HFDesignPatterns01/src/StrategyPaternCharacters/WeaponBehavior.java
@@ -0,0 +1,9 @@
+package StrategyPaternCharacters;
+
+/**
+ * A Character has a weaponbehavior
+ * A weapon implements a Weapon
+ * */
+public interface WeaponBehavior {
+    public void useWeapon();
+}
