0c542032ab5c09c9b406a4ab2eb46a4413c17c6b
Fikasentani <mpilo.dlamini@yahoo.com>
2023-10-03 19:26:58 +0200

    adding condition display

---
+++ HFDesignPatterns02/src/builtInObserverPatternWeather/ConditionDisplay.java
@@ -0,0 +1,36 @@
+package builtInObserverPatternWeather;
+
+
+import ObserverPatternWeather.DisplayElement;
+import ObserverPatternWeather.WeatherData;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ConditionDisplay implements Observer, DisplayElement {
+    Observable observable;
+    float temperature;
+    float humidity;
+
+    public ConditionDisplay(Observable observable) {
+        this.observable = observable;
+        observable.addObserver(this);
+    }
+    @Override
+    public void update(Observable obs, Object arg) {
+        if (obs instanceof WeatherDataBuiltInObserver) {
+            WeatherDataBuiltInObserver weatherDataBuiltInObserver = (WeatherDataBuiltInObserver) obs;
+            this.temperature = weatherDataBuiltInObserver.getTemperature();
+            this.humidity = weatherDataBuiltInObserver.getHumidity();
+            display();
+        }
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
+
+
+
+}
