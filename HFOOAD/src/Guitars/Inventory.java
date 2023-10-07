e6c574b1558c87e1d7ae492ed9d1b629aba27a1e
Fikasentani <mpilo.dlamini@yahoo.com>
2023-10-07 20:40:55 +0200

    adding search guitar

--- HFOOAD/src/Guitars/Inventory.java
+++ HFOOAD/src/Guitars/Inventory.java
@@ -25,4 +25,37 @@ public class Inventory {
         }
         return  null;
     }
+
+    public Guitar Search(Guitar searchGuitar) {
+        for (Iterator i = guitars.iterator(); i.hasNext(); ) {
+            Guitar guitar = (Guitar) i.next();
+            // Ignore serial number since that’s unique
+            // Ignore price since that’s unique
+
+            String builder = searchGuitar.getBuilder();
+
+            if((builder != null) && (!builder.equals("")) && (!builder.equals((guitar.getBuilder()))))
+                continue;
+
+            String model = searchGuitar.getModel();
+
+            if ((model != null) && (!model.equals("")) && (!model.equals(guitar.getModel())))
+                continue;
+
+            String type = searchGuitar.getType();
+            if ((type != null) && (!searchGuitar.equals("")) && (!type.equals((guitar.getType()))))
+                continue;
+
+            String backWood = searchGuitar.getBackWood();
+
+            if ((backWood != null) && (!backWood.equals("")) && (!backWood.equals(guitar.getBackWood())))
+                continue;
+
+            String topWood = searchGuitar.getTopWood();
+            if ((topWood != null) && (!topWood.equals("")) && (!topWood.equals(guitar.getTopWood())))
+                continue;
+        }
+
+          return null;
+    }
 }
