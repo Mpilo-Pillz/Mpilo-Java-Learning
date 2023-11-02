93d54fa620c7b8aa6c82c5b7bd3a3ae9c77cd0b3
Fikasentani <mpilo.dlamini@yahoo.com>
2023-11-02 23:57:14 +0200

    Ny and chicago pizza

---
+++ HFDesignPatterns04/src/com/learning/pizza/NYStyleCheesePizza.java
@@ -0,0 +1,14 @@
+package com.learning.pizza;
+
+public class NYStyleCheesePizza extends Pizza{
+
+    public NYStyleCheesePizza() {
+        name = "NY Style Sauce and Cheese Pizza";
+        dough = "Thin Crust Doough";
+        sauce = "Marinara Sauce";
+
+        toppings.add("Grated Reggiano Cheese");
+    }
+}
+
+
