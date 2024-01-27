b475fe1cedd670ce74e0a03b8256b46fb60b17c5
mpilopillz <mpilo.dlamini@yahoo.com>
2024-01-27 04:56:11 +0200

    starting solution to vowles

---
+++ CodingExercises/src/Vowels.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Vowels {
+    public static void main(String[] args) {
+
+    }
+
+    public int getNumOfVowels(String sentence) {
+        int count = 0;
+        ArrayList<Character> vowels = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+
+        for (char character: sentence.toCharArray()) {
+
+        }
+
+        return count;
+    }
+}
