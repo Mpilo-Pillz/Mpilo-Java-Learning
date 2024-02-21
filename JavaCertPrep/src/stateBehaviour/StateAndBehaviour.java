a661701955525ba6047b7028f30cb0ac7d5a634b
mpilopillz <mpilo.dlamini@yahoo.com>
2024-02-21 22:28:01 +0200

    learning about methods and vars

---
+++ JavaCertPrep/src/stateBehaviour/StateAndBehaviour.java
@@ -0,0 +1,16 @@
+// Instance variables, Class varibables, Local Variables, Parameters
+// Instance Variables is where objects store state and each is unique to every instance of a class
+// Static Variables are Class variables - fields defined with  STATIC keyword
+// There is only ever one instance or one vlue of it
+// static variables are unique to the class rather tha the instance
+
+
+// Methods
+// optional Access Modifier, Parameter list, Exception List
+// Mandatory Return Type, Name, Body
+// Method Signature is name and parameterlist
+
+// Field Shadowing is when an methd has the same name and type as a instance variable
+class StateAndBehaviour {
+    static final float ENGINE_SIZE = 2; // a constant that will never change
+}
