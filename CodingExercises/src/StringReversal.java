9768bf0091ddc10bb83ffcf9cd72a2146265febe
mpilopillz <mpilo.dlamini@yahoo.com>
2024-02-26 22:08:25 +0200

    completing string reversal challenge

--- CodingExercises/src/StringReversal.java
+++ CodingExercises/src/StringReversal.java
@@ -10,8 +10,16 @@ public class StringReversal {
     void reverse(String word) {
         ArrayList<String> reversedStringArray = new ArrayList<>();
         String wordArrayList[] = word.split("");
+        String reveredWord = "";
 
         System.out.println(Arrays.toString(wordArrayList));
+
+        for (int i = wordArrayList.length - 1; i >= 0; i--) {
+            reveredWord += wordArrayList[i];
+        }
+
+        System.out.println("the revered word is " + reveredWord);
+
 
     }
 }
