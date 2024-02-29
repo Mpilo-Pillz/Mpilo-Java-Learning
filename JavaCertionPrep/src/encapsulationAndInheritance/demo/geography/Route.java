b5902333cf552b71700d0a61190f0d4d717839e0
mpilopillz <mpilo.dlamini@yahoo.com>
2024-02-29 19:08:44 +0200

    leanring about inheritache and encapsulation

---
+++ JavaCertionPrep/src/encapsulationAndInheritance/demo/geography/Route.java
@@ -0,0 +1,28 @@
+package encapsulationAndInheritance.demo.geography;
+
+import java.util.List;
+
+public final class Route extends Shape {
+    private final String name;
+    private final List<LatLon> coordinates;
+
+    public Route(String name, LatLon... coordinates) {
+        this.name = name;
+        this.coordinates = List.of(coordinates);
+    }
+
+    @Override
+    protected String convertToWkt() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("LINESTRING (");
+        for (int i = 0; i < coordinates.size(); i++) {
+            LatLon latLon = coordinates.get(i);
+            sb.append(latLon.getLat() + " " + latLon.getLat());
+            if (i < coordinates.size() - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append(")");
+        return sb.toString();
+    }
+}
