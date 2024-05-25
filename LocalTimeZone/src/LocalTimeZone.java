6fd3a2c41fade2ba9858d366bd4f2d9585cb3e51
Fikasentani <mpilo.dlamini@yahoo.com>
2024-05-25 22:37:40 +0200

    printing the time

--- LocalTimeZone/src/LocalTimeZone.java
+++ LocalTimeZone/src/LocalTimeZone.java
@@ -1,9 +1,13 @@
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class LocalTimeZone {
     public static void main(String[] args) {
         ZoneId zoneId = ZoneId.systemDefault();
         System.out.println("Local system timezone: " + zoneId);
         System.out.println("===================Zoned date time");
+        ZonedDateTime zonedDateTime =  ZonedDateTime.now();
+        System.out.println("ZONEDT: " + zonedDateTime
+        );
     }
 }
